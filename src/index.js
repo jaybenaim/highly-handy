26151ed77148a85ca38d0685294375cb08c51def
jaybenaim <benaimjacob@gmail.com>
2024-03-17 22:44:31 -0400

    fixed service workers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import { createRoot } from "react-dom/client";
-import * as serviceWorker from "../public/serviceWorker";
 import App from "./app";
 import "./assets/scss/style.scss";
+import { reportWebVitals } from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
 serviceWorker.register();
+reportWebVitals();
