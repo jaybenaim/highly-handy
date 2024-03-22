ee82e9af9c35f10bcc592a4ea51d6650f933894d
jaybenaim <benaimjacob@gmail.com>
2024-03-22 12:26:02 -0400

    test page view tracking with mixpanel

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import React, { Suspense, lazy } from "react";
+import mixpanel from "mixpanel-browser";
+import React, { Suspense, lazy, useEffect } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { IS_DEVELOPMENT } from "./consts";
 
 const HomeOne = lazy(() => import("./home/HomeOne"));
 const Services = lazy(() => import("./service/Services"));
@@ -10,6 +12,14 @@ const Contact = lazy(() => import("./pages/Contact"));
 const NoMAtch = lazy(() => import("./pages/404"));
 
 const App = () => {
+  useEffect(() => {
+    mixpanel.init("YOUR_TOKEN", {
+      debug: IS_DEVELOPMENT,
+      track_pageview: true,
+      persistence: "localStorage",
+    });
+  }, []);
+
   return (
     <Router>
       <Suspense fallback={<div />}>
--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,5 @@
+export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+
 export const BASE_URL = process.env.REACT_APP_PUBLIC_URL;
 
 export const SERVICES = {
