7cdade0ce9dc8eb46d5a01ea406ccd4074204429
jaybenaim <benaimjacob@gmail.com>
2024-03-22 14:32:51 -0400

    test for auto location

--- src/context/UserLocationProvider.js
+++ src/context/UserLocationProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { IS_DEVELOPMENT, MIXPANEL_EVENTS } from "../consts";
 import { useMixPanel } from "./MixPanelProvider";
@@ -9,56 +9,56 @@ export const useUserLocation = () => useContext(UserLocationContext);
 
 const UserLocationProvider = ({ children }) => {
   const location = useLocation();
-  const [userLocation, setUserLocation] = useState({
-    city: "",
-    country: "",
-  });
+  // const [userLocation, setUserLocation] = useState({
+  //   city: "",
+  //   country: "",
+  // });
   const { mixpanelTrack } = useMixPanel();
 
   const trackPageView = () => {
-    if (!IS_DEVELOPMENT && userLocation) {
+    if (!IS_DEVELOPMENT) {
       const initialReferer = document.referrer || "";
       mixpanelTrack(MIXPANEL_EVENTS.PAGE_VIEW, {
         page: location.pathname,
         referer: initialReferer,
-        $city: userLocation.city,
-        $country: userLocation.country,
+        // $city: userLocation.city,
+        // $country: userLocation.country,
       }).catch((err) => console.log(err));
     } else {
       console.log("Mixpanel disabled");
     }
   };
 
-  useEffect(() => {
-    const getCachedLocation = () => {
-      const cachedLocation = JSON.parse(localStorage.getItem("user_location"));
-      if (cachedLocation) {
-        setUserLocation(cachedLocation);
-        return true;
-      }
-    };
+  // useEffect(() => {
+  //   const getCachedLocation = () => {
+  //     const cachedLocation = JSON.parse(localStorage.getItem("user_location"));
+  //     if (cachedLocation) {
+  //       setUserLocation(cachedLocation);
+  //       return true;
+  //     }
+  //   };
 
-    const fetchUserLocation = async () => {
-      try {
-        const response = await fetch("https://ipapi.co/json/");
-        if (response.ok) {
-          const data = await response.json();
-          const { latitude, longitude, city, country } = data;
-          const locationData = { latitude, longitude, city, country };
-          setUserLocation(locationData);
-          localStorage.setItem("user_location", JSON.stringify(locationData));
-        } else {
-          console.error("Failed to fetch location data");
-        }
-      } catch (error) {
-        console.error("Error getting user location:", error);
-      }
-    };
+  //   const fetchUserLocation = async () => {
+  //     try {
+  //       const response = await fetch("https://ipapi.co/json/");
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         const { latitude, longitude, city, country } = data;
+  //         const locationData = { latitude, longitude, city, country };
+  //         setUserLocation(locationData);
+  //         localStorage.setItem("user_location", JSON.stringify(locationData));
+  //       } else {
+  //         console.error("Failed to fetch location data");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error getting user location:", error);
+  //     }
+  //   };
 
-    const cached = getCachedLocation();
-    !cached && fetchUserLocation();
-    trackPageView();
-  }, []);
+  //   const cached = getCachedLocation();
+  //   !cached && fetchUserLocation();
+  //   trackPageView();
+  // }, []);
 
   useEffect(() => {
     // Listen for location changes and track page view
@@ -66,7 +66,7 @@ const UserLocationProvider = ({ children }) => {
   }, [location.pathname]); // Track page view when location.pathname changes
 
   return (
-    <UserLocationContext.Provider value={{ userLocation }}>
+    <UserLocationContext.Provider value={{}}>
       {children}
     </UserLocationContext.Provider>
   );
