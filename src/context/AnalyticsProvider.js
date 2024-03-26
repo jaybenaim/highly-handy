d58c7b2fbeec8d0d7a98c54433387023c148620f
jaybenaim <benaimjacob@gmail.com>
2024-03-26 17:04:13 -0400

    added vercel speed insights

--- src/context/AnalyticsProvider.js
+++ src/context/AnalyticsProvider.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createContext, useContext } from "react";
 
 const AnalyticsContext = createContext();
@@ -9,6 +10,7 @@ const AnalyticsProvider = ({ children }) => {
   return (
     <AnalyticsContext.Provider value={{}}>
       <Analytics />
+      <SpeedInsights />
       {children}
     </AnalyticsContext.Provider>
   );
